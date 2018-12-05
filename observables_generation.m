dce14fccc25583a5c8f9dc71d1f426f6306bee1d
arnauochoa <arnau.ochoa@gmail.com>
2018-12-05 12:51:00 +0100

    adapted to various Rx

--- observables_generation.m
+++ observables_generation.m
@@ -18,21 +18,22 @@ function [rxTime, rxFreq] = observables_generation(rxPos, rxVel, txPos, txVel, t
 %               rxFreq: Double. Received signal frequency in Hz
 
     %- Parameters initialization
-    c       =   299792458;              % Speed of light (m/s)
+    n       =   1;                      % Refractive index
+    c       =   299792458/n;            % Speed of light (m/s)
     
     %- Relative distance and range rate computation
     [rRate, dRel]   =   compute_range_rate(rxPos, rxVel, txPos, txVel);
     
     %- Actual propagation time and frequency drift computation
     tProp   =   dRel/c;                 % Propagation time
-    fDop    =   txFreq * (rRate/c);      % Doppler frequency drift
+    fDop    =   txFreq * (rRate/c);     % Doppler frequency drift
     
     %- Observed frequency computation
-    fNoise  =   compute_freq_noise(SNR, Ns);  % TODO
+    fNoise  =   compute_freq_noise(SNR, Ns);
     rxFreq  =   txFreq + fDop + fNoise;
     
     %- Observed reception time compution
-    tNoise  =   compute_time_noise(SNR, Ns, rxFreq);  % TODO
+    tNoise  =   compute_time_noise(SNR, Ns, rxFreq);
     rxTime  =   txTime + tProp + tNoise; 
 
 end
--- simulation_script.m
+++ simulation_script.m
@@ -1,27 +1,56 @@
 clear all; close all; clc; %#ok<CLALL>
 
 %- Transmitter parameters
-txPos   =   [50; 50; 0].*1e3;       % Position X-Y-Z [m]
-txVel   =   [10; 100; 0];           % Velocity X-Y-Z [m/s]
-txTime  =   0;                      % Transmission time [s]
-txFreq  =   1575.42;                % Transmission frequency [MHz]
+txPos       =   [50, 50];               % Position X-Y-Z [km]
+txVel       =   [10, 10];               % Velocity X-Y-Z [m/s]
+txTime      =   0;                      % Transmission time [s]
+txFreq      =   1575.42;                % Transmission frequency [MHz]
 
 %- Receiver parameters
-rxPos   =   [0; 0; 0].*1e3;         % Position X-Y-Z [m]
-rxVel   =   [0; 0; 0];              % Velocity X-Y-Z [m/s]
+numRx       =   2;                      % Number of receivers
+dim         =   2;                      % Dimensions
+rxPos       =   zeros(numRx, dim);      % Position matrix [km]
+rxVel       =   zeros(numRx, dim);      % Velocity matrix [m/s]
+rxPos(1, :) =   [10, 5];                % Rx1 position
+rxVel(1, :) =   [0, 0];                 % Rx1 velocity
+rxPos(2, :) =   [10, 60];               % Rx2 position
+rxVel(2, :) =   [0, 0];                 % Rx2 velocity
 
 %- CRB parameters
-SNR_dB  =  40;                     % Signal-to-Noise Ratio [dB]
-Ns      =  15;                     % Number of samples []
+SNR_dB      =  40;                     % Signal-to-Noise Ratio [dB]
+Ns          =  15;                     % Number of samples []
 
+%- Parameters adaptation
 SNR     =   db2pow(SNR_dB);
 txFreq  =   txFreq * 1e6;
-[rxTime, rxFreq] = observables_generation(rxPos, rxVel, txPos, txVel, txTime, txFreq, SNR, Ns);
+txPos   =   txPos.*1e3;
+rxPos   =   rxPos.*1e3;
 
+rxTime  =   zeros(numRx, 1);
+rxFreq  =   zeros(numRx, 1);
+for rx = 1:numRx
+    [rxTime(rx), rxFreq(rx)] = observables_generation(rxPos(rx,:), rxVel(rx,:),...
+        txPos(), txVel(), txTime, txFreq, SNR, Ns);
+end
 
 
 
 % -------------------------------------------------------------------------
-fprintf("\n ------ Results ------\n");
-fprintf(" Reception time: %f seconds \n", rxTime);
-fprintf(" Received signal frequency: %f MHz \n", rxFreq/1e6);
+fprintf("\n ========= Results =========\n");
+for rx = 1:numRx
+    fprintf(" --- Receiver %d ---\n", rx);
+    fprintf(" Reception time: %f seconds \n", rxTime(rx));
+    fprintf(" Received signal frequency: %f MHz \n", rxFreq(rx)/1e6);
+end
+
+scale = 1e3;
+figure;
+scatter(txPos(1), txPos(2), 'r', 'x'); hold on;
+scatter(rxPos(:, 1), rxPos(:, 2), 'b', 'x'); hold on;
+quiver(txPos(:, 1), txPos(:, 2), txVel(:, 1)*scale, txVel(:, 2)*scale); hold on;
+quiver(rxPos(:, 1), rxPos(:, 2), rxVel(:, 1)*scale, rxVel(:, 2)*scale);
+[maxPosX, i] = max(txPos(:,1));
+[maxPosY, j] = max(txPos(:,2));
+xlim([0 maxPosX+txVel(i, 1)*scale+10e3]);
+ylim([0 maxPosY+txVel(j, 2)*scale+10e3]);
+
