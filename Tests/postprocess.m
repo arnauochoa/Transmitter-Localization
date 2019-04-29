2668dc4152d2f8154c815542579ad742e71cbf4c
arnauochoa <arnau.ochoa@gmail.com>
2019-04-29 15:59:18 +0200

    error ellipses added to postprocess

--- Tests/postprocess.m
+++ Tests/postprocess.m
@@ -1,6 +1,8 @@
 clear all;
+close all;
+clc;
 
-testName    =   'tl1';
+testName    =   'tl4';
 
 directory   =   strcat('Results/', testName, '/data.mat');
 
@@ -8,11 +10,15 @@ load(directory);
 
 errorA      =   zeros(var.steps, var.radSteps);
 errorB      =   zeros(var.steps, var.radSteps);
-
+err2dA      =   zeros(var.steps, 2, var.radSteps);
+err2dB      =   zeros(var.steps, 2, var.radSteps);
 for i = 1:var.steps
-    for j = 1:var.radSteps
-       errorA(i, j) = sqrt(sum((tx(i, j).pos - estA(i, j).pos).^2));
-       errorB(i, j) = sqrt(sum((tx(i, j).pos - estB(i, j).pos).^2));
+    for rad = 1:var.radSteps
+       errorA(i, rad) = sqrt(sum((tx(i, rad).pos - estA(i, rad).pos).^2));
+       errorB(i, rad) = sqrt(sum((tx(i, rad).pos - estB(i, rad).pos).^2));
+       
+       err2dA(i, :, rad) = tx(i, rad).pos - estA(i, rad).pos;
+       err2dB(i, :, rad) = tx(i, rad).pos - estB(i, rad).pos;
     end
 end
 
@@ -45,6 +51,36 @@ xlabel('Error [m]');
 legend(radLegend, 'Location', 'southeast'); 
 legend('boxoff');
 title("CDF of RSS/DoA method over radius variation");
+
+scale = 10;
+figure; set(gcf, 'Position',  [400, 50, 950, 900]);
+legend;
+for rad = 2
+    for i = 1:var.steps
+        %- Actual positions
+        name    =   sprintf("Receiver at t=%d", i);
+        scatter(tx(i, rad).pos(1), tx(i, rad).pos(2), 'g', 'x', 'DisplayName', name); hold on;
+        %- Estimated positions
+        %-- TDoA/FDoA
+        name    =   sprintf("(TDoA/FDoA) Estimation at t=%d", i); 
+        scatter(estA(i, rad).pos(1), estA(i, rad).pos(2), 'r', 'x', 'DisplayName', name); hold on;
+        C       =   cov(txEstPosA(:, :, rad, i));
+        error_ellipse(C, estA(i, rad).pos);
+        %-- RSS/DoA
+        name    =   sprintf("(RSS/DoA) Estimation at t=%d", i);
+        scatter(estB(i, rad).pos(1), estB(i, rad).pos(2), 'm', 'x', 'DisplayName', name); hold on;
+        C       =   cov(txEstPosB(:, :, rad, i));
+        error_ellipse(C, estB(i, rad).pos);
+    end
+end
+
+for i = 1:scen.numRx
+    scatter(rx(i).pos(1), rx(i).pos(2), 'b', 'x', 'DisplayName', 'Receivers'); hold on;
+    quiver(rx(i).pos(1), rx(i).pos(2), ...
+        rx(i).vel(1)*scale, rx(i).vel(2)*scale, 'b'); hold on;
+end
+xlabel('x'); ylabel('y');
+
 
 % figure;
 % for i = 1:var.steps
